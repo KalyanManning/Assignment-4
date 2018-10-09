52e6851ce7b3628d48cdc915d4e5f67020ae4d77
KalyanManning <kalyanmanning23@gmail.com>
2018-10-09 15:35:22 +1100

    Completed testing preconditions of roomIdEntered()

--- test/hotel/checkout/Bug1Test.java
+++ test/hotel/checkout/Bug1Test.java
@@ -6,6 +6,7 @@
 package hotel.checkout;
 
 import hotel.HotelHelper;
+import hotel.entities.Booking;
 import static org.junit.Assert.*;
 
 import org.junit.After;
@@ -14,6 +15,8 @@ import org.junit.Test;
 import org.mockito.Mock;
 
 import hotel.entities.Hotel;
+import hotel.entities.ServiceCharge;
+import java.util.List;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
@@ -26,6 +29,8 @@ import org.mockito.runners.MockitoJUnitRunner;
  */
 @RunWith(MockitoJUnitRunner.class)
 public class Bug1Test {
+    
+    int roomId = 301;
     
     @Mock CheckoutUI checkoutUI;
     
@@ -54,8 +59,15 @@ public class Bug1Test {
     public void testRoomIdEntered() {
         
         control.run();
-        control.roomIdEntered(301);
+        control.roomIdEntered(roomId);
         assertEquals(7.00, control.total, 0.1);
     }
     
+    @Test
+    public void testRoomIdEnteredPreconditions(){
+        Booking booking = hotel.findActiveBookingByRoomId(roomId);
+        List<ServiceCharge> charges = booking.getCharges();
+        double actual = charges.get(0).getCost();
+        assertEquals(7.00, actual, 0.1);
+    }
 }
